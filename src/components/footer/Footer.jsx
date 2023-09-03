6455c3912ccd792f785a38b332a576d8e0a3e687
Gabo250 <ggaborr1@gmail.com>
2023-09-03 12:05:09 +0200

    some changes in components and deleting app.css file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,18 +4,18 @@ import Tooltip from "./Tooltip";
 function Footer() {
     return (
         <footer id="conn" className="h-[30rem] xmd:h-[40rem] w-full bg-ftbg bg-cover z-[1]">
-            <section className="fixed flex flex-row xmd:flex-col justify-center gap-[10vw] xmd:gap-8 items-center top-[calc(100%-27rem)] rounded-2xl
+            <section className="fixed xsm:relative flex flex-row xmd:flex-col justify-center gap-[10vw] xmd:gap-8 items-center top-[calc(100%-27rem)] rounded-2xl
                                 xmd:top-[calc(100%-37.5rem)] left-[50%] -translate-x-[50%] h-[25rem] xmd:h-[35rem] w-4/5
                                 xmd:w-11/12 xsm:!w-full backdrop-blur-lg z-[1] shadow-2xl shadow-purple-600">
                 
                 <div className="flex flex-col items-center xmd:flex-row xsm:!flex-col xsm:!gap-8 xmd:gap-[10vw] gap-10 mt-1">
                     <div className="flex flex-row items-center justify-center gap-8 border-dashed border-b-[1px] xmd:border-b-0 border-purple-300 xmd:border-r-[1px]
                                     pb-10 xmd:pb-0 xmd:pr-16 xsm:!border-r-0 xsm:!border-b-[1px] xsm:!pb-10 xsm:!pr-0">
-                        <a target="_blank" rel="noreferrer external" href="https://www.facebook.com/profile.php?id=100091972387797" className="relative group">
+                        <a target="_blank" aria-label="Check my Instagram page" rel="noreferrer external" href="https://www.facebook.com/profile.php?id=100091972387797" className="relative group">
                             <FBIcon classes={"hover:scale-110 transition-all drop-shadow-[0px_5px_6px_#4444dd]"}/>
                             <Tooltip>Facebook</Tooltip>
                         </a>
-                        <a target="_blank" rel="noreferrer external" href="https://www.instagram.com/gyemantkucko/" className="relative group">
+                        <a target="_blank" aria-label="Check my Facebook page" rel="noreferrer external" href="https://www.instagram.com/gyemantkucko/" className="relative group">
                             <InstaIcon classes={"hover:scale-110 transition-all drop-shadow-[0px_5px_6px_#C13584]"}/>
                             <Tooltip>Instagram</Tooltip>
                         </a>
--- src/components/header/About.jsx
+++ src/components/header/About.jsx
@@ -9,23 +9,31 @@ function About() {
 
     return (
         <article className="relative flex flex-row gap-10 w-4/5 xmd:w-11/12 justify-evenly items-center xmd:flex-col z-[2]">
-            <div className="relative w-52 h-[290px]">
+            <div className="relative h-[290px]">
                 <img ref={iRef} className={`rounded-full w-full h-full ${iInViewbox ? "animate-fadein" : ""}`} src="./img/vik.jpg" alt="Nagy Viktória - Masszőr, Lélekgyógyász" />
             </div>
 
             <div className="w-1/2 xmd:w-11/12">
-                <h2 className="text-justify leading-6">
-                    <span ref={nRef} className={`font-extrabold text-3xl relative text-transparent
-                                                ${ nInViewBox ? "after:animate-revealright" : "" }
-                                                after:content-['Nagy_Viktória'] after:absolute after:-top-[.2px] after:left-[.6px]
-                                                after:w-full after:text-purple-500 after:whitespace-nowrap after:overflow-hidden
-                                                after:[text-shadow:0px_0px_5px_#ffffff]`}>
-                        Nagy Viktória
-                    </span>
-                    &nbsp;vagyok, masszőr és lélekgyógyító.
-                    Az én szenvedélyem az emberek segítése abban, hogy testben és lélekben egyaránt harmóniára találjanak.
-                    Hiszek abban, hogy az egészség és a boldogság összefonódik,
-                    és hogy a testi-lelki egyensúly eléréséhez szükséges az odafigyelés és a tudatos gondoskodás.
+                <h2 className="text-justify leading-6">                   
+                    <p>
+                        <span ref={nRef} className={`font-extrabold text-3xl mr-2 relative text-transparent
+                                                    ${ nInViewBox ? "after:animate-revealright" : "" }
+                                                    after:content-['Nagy_Viktória'] after:absolute after:-top-[.2px] after:left-[.6px]
+                                                    after:w-full after:text-purple-500 after:whitespace-nowrap after:overflow-hidden
+                                                    after:[text-shadow:0px_0px_5px_#ffffff]`}>
+                            Nagy Viktória
+                        </span>
+                        vagyok, masszőr és lélekgyógyító.
+                        Az én szenvedélyem az emberek segítése abban, hogy testben és lélekben egyaránt harmóniára találjanak.
+                        Hiszek abban, hogy az egészség és a boldogság összefonódik,
+                        és hogy a testi-lelki egyensúly eléréséhez szükséges az odafigyelés és a tudatos gondoskodás.
+                    </p>
+                    <p className="mt-3">
+                        Masszázskezeléseimben ötvözöm a hagyományos technikákat és az intuitív megközelítést,
+                        hogy megteremtsem a teljes ellazulás és regeneráció élményét. Mindegy, hogy csak egy pillanatnyi kikapcsolódásra vágysz,
+                        vagy hosszabb távú stresszoldásra és energetikai feltöltődésre van szükséged,
+                        személyre szabott kezeléseimet a te igényeidre és testi állapotodra szabom.
+                    </p>
                 </h2>
             </div>
         </article>        
