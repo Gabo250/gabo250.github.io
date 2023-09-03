726a3d4a213601aa43aa2a7db89b9592e54d227e
Gabo250 <ggaborr1@gmail.com>
2023-09-03 09:16:42 +0200

    price and service some title style change

--- src/components/pricecomp/PriceList.jsx
+++ src/components/pricecomp/PriceList.jsx
@@ -5,9 +5,9 @@ function PriceList() {
     return (
             <section id="price" className="relative grid grid-cols-[repeat(2,minmax(20rem,30rem))] justify-center lmd:grid-cols-1 pb-32 pt-24 gap-y-24 gap-x-16 z-[2]
                                             bg-gradient-to-r from-slate-900  via-slate-700 to-slate-900">            
-                <header className="relative col-span-2 lmd:col-span-1 h-16 shadow-2xl shadow-slate-600">                      
+                <header className="relative col-span-2 lmd:col-span-1 h-16">                      
                     <h2 className="relative text-[36px] text-center [text-shadow:2px_2px_4px_#000000] align-middle top-[50%]
-                                    -translate-y-[50%] h-full border-solid border-b-[1px] border-slate-400 border-opacity-60"
+                                    -translate-y-[50%] h-full drop-shadow-[0px_15px_10px_#0b0d0e]"
                     >Árak</h2>    
                 </header>
 
--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -5,9 +5,9 @@ import ServiceCard from "./ServiceCard";
 function Service() {
     return(
         <section id="serv" className="relative w-full flex flex-col items-center gap-32 pt-24 pb-32 z-[2] bg-gradient-to-r from-black  via-purple-950 to-black">
-            <header className="relative w-[max(60rem,300px)] h-16 shadow-2xl shadow-purple-800 border-b-[1px]
-                            border-purple-400 border-opacity-60">
-                <h2 className="relative text-4xl text-center [text-shadow:2px_2px_4px_#000000] h-14 border-solid top-[50%] -translate-y-[50%]"
+            <header className="relative w-[max(60rem,300px)] h-16">
+                <h2 className="relative text-4xl text-center [text-shadow:2px_2px_4px_#000000] h-14 border-solid top-[50%] -translate-y-[50%]
+                                drop-shadow-[0px_15px_15px_#800080]"
                 >Szolgáltatások</h2>
             </header>
             
