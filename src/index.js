bb4ba5684b2ab72febde4c1ec79b328fb38834a4
Gabo250 <ggaborr1@gmail.com>
2023-10-25 10:48:51 +0200

    index.js changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App />
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+    ReactDOM.hydrateRoot(rootElement).render(<App />);
+}
+else {
+    ReactDOM.createRoot(rootElement).render(<App />);
+}
