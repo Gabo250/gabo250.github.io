2b66ffb3620f1762177e0e5660431acefcf096bf
Gabo250 <ggaborr1@gmail.com>
2024-01-30 09:47:25 +0100

    update

--- src/components/footer/Tooltip.jsx
+++ src/components/footer/Tooltip.jsx
@@ -2,8 +2,8 @@ function Tooltip({ children }) {
     return (
       <span
         className="absolute hidden font-boo group-hover:block left-[50%] group-hover:animate-fadein
-                        group-hover:[animation-duration:.5s] -translate-x-[50%] py-1 px-3 -top-[2.5rem] border-solid border-x-[1px]
-                        border-y-[1px] border-black border-opacity-50rounded-lg bg-slate-600 bg-opacity-50 tooltip-after"
+                    group-hover:[animation-duration:.5s] -translate-x-[50%] py-1 px-3 -top-[2.5rem] border-solid border-x-[1px]
+                    border-y-[1px] border-black border-opacity-50rounded-lg bg-slate-600 bg-opacity-50 tooltip-after"
       >
         {children}
       </span>
